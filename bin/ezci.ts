14cd16e42041e0e14bb1adfb363b6b4102dd7bf6
Johannes Weih <sheepr4ider@gmail.com>
2021-12-28 14:03:51 +0100

    Use metadata from package.json

--- bin/ezci.ts
+++ bin/ezci.ts
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 import { program } from '@caporal/core';
 import { runAction } from '../commands/run/run';
+import { description, name, version } from '../package.json';
 
 const bootstrap = (): void => {
   program
+    .name(name)
+    .version(version)
+    .description(description)
     .command('run', 'Run CI pipeline', {})
     .option('--branch-pattern, -p [branch-pattern]', 'Pattern of branches hooked to CI triggers', { default: 'release/' })
     .action(runAction);
